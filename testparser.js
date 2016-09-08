5d0be0c83fe8492531c286fc115393ea06545978
Gilles Coomans <gilles.coomans@gmail.com>
2016-09-08 18:18:32 +0200

    work on testparser.js

--- testparser.js
+++ testparser.js
@@ -37,25 +37,38 @@ Babelute.toLexic('bar', {
 });
 
 // var b = Babelute.parse('div(function(a, b){ if(e){ b = {}; }else{}  return a + b; })')
-var b = Babelute.parse(`
-	foo:
-		goo(
-			1, 
-			goo() 
-			bar:
-				zoo(true)
-			foo:
-				goo(1)
-		) 
+// var b = Babelute.parse(`
+// 	foo:
+// 		goo(s
+// 			1, 
+// 			goo() 
+// 			bar:
+// 				zoo(true)
+// 			foo:
+// 				goo(1)
+// 		) 
 
-	bar:
-		zoo(true)
+// 	bar:
+// 		zoo(true)
 
-`, {
-	asDoc: false,
-	acceptFunctions: true
-})
+// `, {
+// 	asDoc: false,
+// 	acceptFunctions: true
+// })
 
-console.log('b : ', b && b.stringify());
+// console.time('Babelute parse');
+// for (var i = 0; i < 10000; ++i)
+
+// var b = Babelute.parse('foo:goo("hello\\"world") goo(true, 1, null) goo(true, 1,  goo(true, 1,  goo(true, 1, null)))  goo(true, 1, null)')
+var b = Babelute.parse('foo:goo("hello\\"world")')
+console.log('\nb : ', b && b.stringify());
+console.log('\n\ncounts : ', Babelute.Parser.counts, '\n');
+
+
+
+// console.timeEnd('Babelute parse');
+
+// var b = Babelute.parse('foo:goo("hello\\"world") bar:zoo(true)')
+// console.log('b : ', b && b.stringify());
 
 // console.log('b', JSON.stringify(b))
