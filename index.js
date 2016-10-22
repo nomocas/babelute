05ea6ac1260e166ce3c628dd1df38776a8883feb
Gilles Coomans <gilles.coomans@gmail.com>
2016-10-22 11:30:38 +0200

    work on todo in index.js

--- index.js
+++ index.js
@@ -26,6 +26,74 @@
  * @copyright 2016 Gilles Coomans
  */
 
+
+/**
+ * Todo :
+ *
+ * 		debug translation 				OK
+ *
+ * 		_append : should add lexicName : 		OK
+ *
+ * 		on add to lexic(lexicName) :      ==> 	OK 
+ * 		first time : create immediatly Babelute children class
+ * 		after : modify existing prototypes (babelute, docs, and dothat if any) in place of clearing cache
+ *
+ * 		==> same thing when extending lexic : use inheritance to keep tracking of parent prototype    	==> 	OK
+ *
+ * 		scoped lexics management on stringify  				OK
+ * 		
+ * 		manage restrictions with mixins when extending Lexics/Actions/API  					OK
+ *
+ *		scope management
+ *			use env.scope() to get current scope object or maybe pass it as fourth argument in actions
+ *			env.pushScope({ name:instance })
+ *			env.popScope(name)
+ *
+ *		$output({
+ *			actions:'yamvish:dom',
+ *			scope:{
+ *				context: context || new Context(...)
+ *			}
+ *		})
+ * 
+ * 
+ * 		finalise time management in actions
+ * 			maybe add flag in actions namespace to say 'allowAsync'
+ * 		manage result forwarding and promise listening 
+ *    	 		
+ * 		do simple example with async manager in env
+ *
+ *		manage in-lexem-actions
+ * 
+ *		translation/output table
+ * 
+ *
+ * 		//_____________ after
+ * 
+ * 
+ *		add Filter style with @myFunc
+ *
+ * 		add def mecanism  with dereferencement (aka $myVar)
+ *
+ *		work on babelute doc pilot : external query DSL ? api ?
+ *  		.0 = args[0]
+ *  		.name = select lexems with matching name
+ *    		.#lexic = select lexems with matching lexic
+ *    		.#lexic:lexem = select lexems with matching lexic and lexem name
+ *    		.*
+ *    		.*(filter)
+ *    		.**(0=is=babelute)
+ *    	 	.**(div|span|#foo:goo.0=is=babelute)
+ * 		
+ * 		extract yaom
+ *
+ * 		extract dothat
+ * 
+ * 		bridge between babelute actions and dothat API
+ *
+ * 		add tests
+ */
+
 // core class and statics
 var Babelute = require('./lib/babelute');
 
