a4f3a2abc8d461793402c674aba49214fa841792
Gilles Coomans <gilles.coomans@gmail.com>
2016-10-22 11:30:16 +0200

    work on index.js doc comment

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 /**
  * Babelute.
- * Method chaining applications to Templating and Domain Language (multi)Modeling.
+ * Javascript Internal DSMM Framework.
+ * Method chaining applications to Templating and Internal Domain Specific (Multi)Modeling.
+ * Aka demonstration that everything is about languages.
  * 
  * Domain Language (Multi)Modeling solves many software design problems.
- * From developpement process (how to start, what to design, how to design, how to iterate, ...) 
+ * From developpement process (how to start, what and how to design, how to iterate, ...) 
  * to how to understand, design and articulate business related problems and/or pure code logics.
  * 
  * It provides natural ways of thinking models and code that are really close to how human mind 
- * works. Much much more close than OO (including AOP) is.
+ * works. Much much more close than OOP (including AOP) is.
  *
  * Babelute gives elegant and really simple ways to define/handle/use/transform chainable methods
- * in pure javascript or through it's own simple DSL
- * (a minimalist optimal string representation of call chains for writing/serialising babelute 
- * sentences and/or lexics).
+ * in pure javascript or through it's own simple external DSL (a minimalist optimal string representation 
+ * of call chains for babelute sentences serialisation).
  * 
  * It relays entierly on Method Chaining pseudo design pattern.
  * In consequence, it's made with really few code, and is really light (less than 2ko gzip/min without own 
- * DSL parser - which is also really light (+- 1ko gzip/min))
+ * external DSL parser - which is also really light (+- 1ko gzip/min) and made with its dedicated Fluent Interface)
  *
  * Babelute's parsing and output are both really fast. 
- * The pattern and method are already heavily used in other fast libs, that solve elegantly really 
- * disconnected problems,  
- * that have shown that was a really good approach to have both 
- * expressivness and performance (jquery (zepto, sprint), elenpi, aright, yamvish, deepjs chains, ...).
  *
  * @author Gilles Coomans
  * @licence MIT
