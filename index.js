9e6e6cf4c2d804451372cc13121e23054ced0314
Gilles Coomans <gilles.coomans@gmail.com>
2016-12-13 00:28:32 +0100

    finalise index

--- index.js
+++ index.js
@@ -2,110 +2,30 @@
  * Babelute.
  * Javascript Internal DSMM Framework.
  * Method chaining applications to Templating and Internal Domain Specific (Multi)Modeling.
- * Aka demonstration that everything is about languages.
  * 
- * Domain Language (Multi)Modeling solves many software design problems.
+ * Domain Specific (Multi)Modeling (using DSL at all levels) solves many software design problems.
  * From developpement process (how to start, what and how to design, how to iterate, ...) 
  * to how to understand, design and articulate business related problems and/or pure code logics.
  * 
- * It provides natural ways of thinking models and code that are really close to how human mind 
- * works. Much much more close than OOP (including AOP) is.
- *
- * Babelute gives elegant and really simple ways to define/handle/use/transform chainable methods
- * in pure javascript or through it's own simple external DSL (a minimalist optimal string representation 
- * of call chains for babelute sentences serialisation).
- * 
  * It relays entierly on Method Chaining pseudo design pattern.
- * In consequence, it's made with really few code, and is really light (less than 2ko gzip/min without own 
- * external DSL parser - which is also really light (+- 1ko gzip/min) and made with its dedicated Fluent Interface)
- *
- * Babelute's parsing and output are both really fast. 
+ * In consequence, Babelute itself is made with really few code, and is really light (about 2.5ko gzip/min without own 
+ * external DSL parser and serialiser - which are also really light (+- 3ko gzip/min)).
  *
  * @author Gilles Coomans
  * @licence MIT
  * @copyright 2016 Gilles Coomans
  */
 
-
-/**
- * Todo :
- *
- * 		debug translation 				OK
- *
- * 		_append : should add lexicName : 		OK
- *
- * 		on add to lexic(lexicName) :      ==> 	OK 
- * 		first time : create immediatly Babelute children class
- * 		after : modify existing prototypes (babelute, docs, and dothat if any) in place of clearing cache
- *
- * 		==> same thing when extending lexic : use inheritance to keep tracking of parent prototype    	==> 	OK
- *
- * 		scoped lexics management on stringify  				OK
- * 		
- * 		manage restrictions with mixins when extending Lexics/Actions/API  					OK
- *
- *		scope management
- *			use env.scope() to get current scope object or maybe pass it as fourth argument in actions
- *			env.pushScope({ name:instance })
- *			env.popScope(name)
- *
- *		$output({
- *			actions:'yamvish:dom',
- *			scope:{
- *				context: context || new Context(...)
- *			}
- *		})
- * 
- * 
- * 		finalise time management in actions
- * 			maybe add flag in actions namespace to say 'allowAsync'
- * 		manage result forwarding and promise listening 
- *    	 		
- * 		do simple example with async manager in env
- *
- *		manage in-lexem-actions
- * 
- *		translation/output table
- * 
- *
- * 		//_____________ after
- * 
- * 
- *		add Filter style with @myFunc
- *
- * 		add def mecanism  with dereferencement (aka $myVar)
- *
- *		work on babelute doc pilot : external query DSL ? api ?
- *  		.0 = args[0]
- *  		.name = select lexems with matching name
- *    		.#lexic = select lexems with matching lexic
- *    		.#lexic:lexem = select lexems with matching lexic and lexem name
- *    		.*
- *    		.*(filter)
- *    		.**(0=is=babelute)
- *    	 	.**(div|span|#foo:goo.0=is=babelute)
- * 		
- * 		extract yaom
- *
- * 		extract dothat
- * 
- * 		bridge between babelute actions and dothat API
- *
- * 		add tests
- */
-
-// core class and statics
+// core class
 var Babelute = require('./lib/babelute');
-
-// Babelute First Degree
-require('./lib/first-level-babelute');
+// lexics
+require('./lib/lexics');
+// actions
+require('./lib/actions');
 
 // serializer to Babelute DSL
 require('./lib/stringify');
-
 // Babelute DSL parser
 Babelute.parser = require('./lib/parser');
-
-
 
 module.exports = Babelute;
